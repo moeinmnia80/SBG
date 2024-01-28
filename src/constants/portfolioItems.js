0ebde34fa798cd628bc0d008a4ac3dcadaaac4d9
moein mohammadnia <moeinmohammadnia@yahoo.com>
2024-01-28 18:48:56 +0330

    portfolio

---
+++ src/constants/portfolioItems.js
@@ -0,0 +1,11 @@
+import ttmpImg from "../assets/images/ttmp.png";
+const portfolioItems = [
+  {
+    id: 1,
+    link: "https://monet-teacher-time-management.netlify.app/",
+    gitLink: "https://github.com/moeinmnia80/univercity-project",
+    img: ttmpImg,
+  },
+];
+
+export default portfolioItems;

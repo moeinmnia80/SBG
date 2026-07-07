f0167c8c2ce6cf792229494675061f7c86a9f162
moein mohammadnia <moeinmohammadnia@yahoo.com>
2026-07-07 21:11:52 +0330

    fix bug for deploy

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { CrossIcon } from "../assets/icons/CrossIcon.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTab } from "../features/project/projectSlice.js";
@@ -8,8 +9,8 @@ export const PortfolioItem = ({ item }) => {
   return (
     <>
       <span
-        className={`flex items-center w-max h-full border-[1px] border-black sm:py-1 px-2 \
-         rounded-md cursor-pointer hover:bg-black hover:text-[#eee] duration-500 \ 
+        className={`flex items-center w-max h-full border-[1px] border-black sm:py-1 px-2
+         rounded-md cursor-pointer hover:bg-black hover:text-[#eee] duration-500 
          ${
            project.id === item.id
              ? `bg-gradient-to-br from-[#FE5858] to-[#EE9617] text-[#eee]`
